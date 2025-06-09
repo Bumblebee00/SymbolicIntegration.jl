dc778324100dd7fa17992a8e201a1a11ecfe7840
Bumblebee00 <mikamatto03@gmail.com>
2025-06-09 21:55:03 +0200

    added the integrate function

--- src/SymbolicIntegration.jl
+++ src/SymbolicIntegration.jl
@@ -1,5 +1,38 @@
 module SymbolicIntegration
 
-# Write your package code here.
+using Symbolics
+
+export integrate
+
+file1 = joinpath(@__DIR__, "IntegrationRules/1 Algebraic functions/1.1 Binomial products/1.1.1 Linear/1.1.1.1 (a+b x)^m.jl")
+rules_paths = [ file1, ]
+
+# for path in testset_paths
+#     if !isfile(path)
+#         error("Rule file not found: ", relpath(path))
+#     end
+#     
+#     # include(path) ? 
+# end
+
+function integrate(integrand, integration_variable::Num)
+    return integration_variable
+end
+
+# If no integration variable provided
+function integrate(integrand)
+    vars = Symbolics.get_variables(integrand)
+    if length(vars) > 1
+        error("Multiple symbolic variables detect. Please pass the integration variable to the `integrate` function as second argument.")
+    elseif length(vars) == 1
+        integration_variable = vars[1]
+    else
+        @variables x
+        integration_variable = x
+    end
+
+    integrate(integrand, integration_variable)
+
+end
 
 end
