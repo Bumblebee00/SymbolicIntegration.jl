2faa38e81caa4deb6ea9c92aacf731e25dc651c9
Bumblebee00 <mikamatto03@gmail.com>
2025-06-22 11:29:09 +0200

    added the testfile each_rule_tests that will contain a test for each rule

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
-using SymbolicIntegration
 using Test
 using Symbolics
+using SymbolicIntegration
 
 # Parse command line arguments
 verbs = false  # default value
@@ -22,15 +22,15 @@ test1_1_1_2 = joinpath(@__DIR__, "test_files/1 Algebraic functions/1.1 Binomial 
 test1_1_1_3 = joinpath(@__DIR__, "test_files/1 Algebraic functions/1.1 Binomial products/1.1.1 Linear/1.1.1.3 (a+b x)^m (c+d x)^n (e+f x)^p.jl")
 test_0_Bronstein = joinpath(@__DIR__, "test_files/0 Independent test suites/Bronstein Problems.jl")
 easy = joinpath(@__DIR__, "test_files/easy_tests.jl")
-testset_paths = [ easy ]
+each_rule = joinpath(@__DIR__, "test_files/each_rule_tests.jl")
+testset_paths = [ each_rule ]
 
 println("About to test SymbolicIntegration.jl with ", length(testset_paths), " test sets")
 
 # analytics for all the testsets
-total_total = 0
-total_n_failed = 0
-total_total_time = 0
-
+total_tests = 0
+total_failed = 0
+total_time = 0
 
 for path in testset_paths
     if !isfile(path)
@@ -43,7 +43,7 @@ for path in testset_paths
     println("Testing ", length(data), " integrals...") # data is the array definied in the test file
 
     # analytics for this testeset
-    n_failed = 0
+    testfile_failed = 0
     times = Float64[]
     
     for (i, test) in enumerate(data)
@@ -55,30 +55,33 @@ for path in testset_paths
                 printstyled("[ ok ]∫( ", test.integrand, " )d", test.integration_var, " = ", test.result, " (", round(elapsed_time, digits=4), "s)\n"; color = :green)
             else
                 printstyled("[fail]∫( ", test.integrand, " )d", test.integration_var, " = ", test.result, " but got ", computed_result, " (", round(elapsed_time, digits=4), "s)\n"; color = :red)
-                n_failed += 1
+                testfile_failed += 1
             end
         catch exceptionz
             printstyled("[fail] exception during ∫( ", test.integrand, " )d", test.integration_var, " : ", exceptionz, "\n"; color=:magenta)
-            n_failed += 1
+            testfile_failed += 1
         end
         
     end
 
-    total_time = sum(times)
-    avg_time = total_time / length(data)
+    testfile_time = sum(times)
+    avg_time = testfile_time / length(data)
     max_time = maximum(times)
     min_time = minimum(times)
     
-    println("$n_failed/$(length(data)) tests failed in ", relpath(path))
+    println("$testfile_failed/$(length(data)) tests failed in ", relpath(path))
     println("Total=$(round(total_time, digits=3))s, Avg=$(round(avg_time, digits=4))s, Min=$(round(min_time, digits=4))s, Max=$(round(max_time, digits=4))s\n\n\n")
 
-    global total_total += length(data)
-    global total_total_time += total_time
-    global total_n_failed += n_failed
+    global total_tests += length(data)
+    global total_time += testfile_time
+    global total_failed += testfile_failed
 end
 
 println("="^80)
 println("="^80)
 println("="^80)
-println("$total_n_failed/$total_total tests failed in all testsets")
-println("Total=$(round(total_total_time, digits=3))s, Avg=$(round(total_total_time / total_total, digits=4))s\n")
+println("$total_failed/$total_tests tests failed in all testsets")
+println("Total=$(round(total_time, digits=3))s, Avg=$(round(total_time / total_tests, digits=4))s\n")
+
+
+@test total_failed == 0
