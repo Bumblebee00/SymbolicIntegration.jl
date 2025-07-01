1845acee1fd5355dfb86667867e4349fb798b03f
Bumblebee00 <mikamatto03@gmail.com>
2025-07-01 18:19:31 +0200

    refactors

--- src/SymbolicIntegration.jl
+++ src/SymbolicIntegration.jl
@@ -1,7 +1,6 @@
 module SymbolicIntegration
 
 using Symbolics
-using SymbolicUtils.Rewriters
 
 @syms ∫(var1,var2) subst(var1, var2, var3)
 # subst is just for when integral inside the real subst function is not solved
